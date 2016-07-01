e1f6ae55ae9303863f579d8f23fb0f056ba7ae02
Paul Trampert <paul.trampert@gmail.com>
2016-07-01 01:02:00 -0400

    Trim down dependencies.

--- src/dotnet-version/Program.cs
+++ src/dotnet-version/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.DotNet.ProjectModel;
 using Newtonsoft.Json;
-using NuGet.Frameworks;
-using NuGet.Versioning;
 
 namespace dotnet_version
 {
