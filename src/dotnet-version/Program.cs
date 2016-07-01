9b6d62144728429558776cdee6af629991f17bcf
Paul Trampert <paul.trampert@gmail.com>
2016-06-30 23:22:48 -0400

    Made the version app output current directory

---
+++ src/dotnet-version/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_version
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetCurrentDirectory());
+        }
+    }
+}
