f5c601e6306eb439b55141671b0f0d3b3d73f684
Paul Trampert <paul.trampert@gmail.com>
2016-07-01 15:06:20 -0400

    Added console logging.

--- src/dotnet-version/Program.cs
+++ src/dotnet-version/Program.cs
@@ -1,18 +1,41 @@
 ﻿using System;
 using System.IO;
 using Microsoft.DotNet.ProjectModel;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace dotnet_version
 {
     public class Program
     {
+        private static readonly ILogger Logger = new LoggerFactory().AddConsole().CreateLogger<Program>();
         public static void Main(string[] args)
         {
-            var projectFilePath = $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}{Project.FileName}";
-            dynamic projectFileText = JsonConvert.DeserializeObject(File.ReadAllText(projectFilePath));
-            projectFileText.version = args[0];
-            File.WriteAllText(projectFilePath, JsonConvert.SerializeObject(projectFileText, Formatting.Indented));
+            if (args.Length == 0 || string.Equals(args[0], "-help", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Usage: dotnet version <VERSION_STRING>");
+                return;
+            }
+            try
+            {
+                var projectFilePath =
+                    $"{Directory.GetCurrentDirectory()}{Path.DirectorySeparatorChar}{Project.FileName}";
+                Logger.LogInformation($"Loading project from {projectFilePath}...");
+                dynamic projectFile = JsonConvert.DeserializeObject(File.ReadAllText(projectFilePath));
+                Logger.LogInformation($"Project loaded.");
+                projectFile.version = args[0];
+                Logger.LogInformation($"Version set to {args[0]}");
+                File.WriteAllText(projectFilePath, JsonConvert.SerializeObject(projectFile, Formatting.Indented));
+                Logger.LogInformation($"Project saved.");
+            }
+            catch (FileNotFoundException e)
+            {
+                Logger.LogError(new EventId(1, "Project file not found"), e, "The project file was not found. Make sure you are running this command from the project root.");
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(new EventId(-1, "Unknown Error"), e, "Unexpected exception occurred.");
+            }
         }
     }
 }
