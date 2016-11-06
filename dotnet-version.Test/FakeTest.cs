daac549b5aff6e0e5bf2be8fff4f9add4b28281e
Paul Trampert <paul.trampert@gmail.com>
2016-11-06 11:48:45 -0500

    Test project to make pipeline happy.

---
+++ dotnet-version.Test/FakeTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace dotnet_version.Test
+{
+    public class FakeTest
+    {
+        [Test]
+        public void True()
+        {
+            Assert.Pass();
+        }
+    }
+}
